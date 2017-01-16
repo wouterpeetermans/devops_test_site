fd0ed122e7715c34728b4cce6fbeac7ff24d0001
user <user>
2017-01-16 12:54:32 +0100

    putting in git

---
+++ client/index.js
@@ -0,0 +1,96 @@
+var serverAddress = "http://localhost"
+
+var app = angular.module('examenPrepApp',['ngRoute']);
+
+app.controller('mainController',function($scope, $location){
+  $scope.goToLogin = function(){
+    $location.path('/login')
+  };
+  $scope.gotoRegister = function () {
+    $location.path('/register')
+  }
+});
+app.controller('loginController',function($scope, $location, $http){
+  $scope.user = {
+    username: "",
+    password: ""
+  };
+  $scope.login = function () {
+    $http.post(serverAddress+"/api/checkUser", $scope.user).success(function (res) {
+      if (res == null) {
+        $location.path('/');
+      } else {
+        $location.path('/home/'+$scope.user.username);
+      }
+    });
+  }
+});
+
+app.controller('registerController',function ($scope, $location, $http) {
+  $scope.user = {
+    username: "",
+    password: ""
+  };
+  $scope.createUser = function () {
+    $http.post(serverAddress+"/api/createUser", $scope.user).success(function (res) {
+      console.log(res);
+      if (!res) {
+        $location.path('/home/'+$scope.user.username);
+        console.log("new user")
+      } else {
+        $scope.info = "deze gebruiker bestaat al";
+      }
+    });
+  }
+
+
+})
+
+app.controller('homeController',function($scope, $location, $routeParams, $interval ,$http){
+  $scope.id = $routeParams.id;
+  $scope.posts = [];
+  $scope.bericht = "";
+  getData();
+  $interval(getData, 1000);
+
+  function getData() {
+    $http.get(serverAddress+"/api/getPosts").success(function (res) {
+      $scope.posts = res;
+    });
+  }
+
+  $scope.sendPost = function () {
+    $scope.post = {"username":$scope.id , "message":$scope.bericht};
+    $http.post(serverAddress+"/api/sendPost",$scope.post).success(function (res) {
+      getData();
+    })
+  }
+
+});
+
+app.config(function($routeProvider){
+  $routeProvider
+    .when('/',{
+      templateUrl: 'main.html',
+      controller: 'mainController'
+    })
+
+    .when('/login',{
+      templateUrl: 'login.html',
+      controller: 'loginController'
+    })
+
+    .when('/home/:id',{
+      templateUrl: 'home.html',
+      controller: 'homeController'
+    })
+
+    .when('/register',{
+      templateUrl: 'register.html',
+      controller: 'registerController'
+    })
+
+    .otherwise({
+      redirectTo:'/'
+    })
+});
---
+++ server.js
@@ -0,0 +1,79 @@
+var express = require('express');
+var bodyparser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+
+var app = express();
+var db;
+var postTable;
+var userTable;
+
+MongoClient.connect('mongodb://localhost:27017/examenPrep', function(err, _db) {
+	if(err) throw err;
+	db = _db;
+	postTable = db.collection('posts');
+  userTable = db.collection('users');
+  console.log("conected to mongodb database");
+});
+
+process.on('SIGINT', function() {
+	db.close();
+  console.log("quited database conection in a proper manner");
+	process.exit(0);
+});
+
+
+app.use(express.static('client'));
+app.use(bodyparser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/api/getPosts', function(req,res){
+  postTable.find().sort({_id:-1}).toArray(function(err, result){
+    if (err) throw err;
+    res.status(200).json(result);
+  });
+});
+
+app.post('/api/sendPost', function(req,res){
+  postTable.insert(req.body,function (err, result) {
+    if (err) throw err;
+    res.status(201).json(null);
+  });
+});
+
+app.post('/api/checkUser', function(req,res){
+  userTable.find({"username":req.body.username}).toArray(function(err ,result){
+    if (err) throw err;
+    if (result[0]) {
+      if (result[0].password == req.body.password) {
+        res.status(200).json(result);
+      } else {
+        res.status(200).json(null);
+      }
+    } else {
+      res.status(200).json(null);
+    }
+  });
+});
+
+app.post('/api/createUser', function(req,res){
+  userTable.find({"username":req.body.username}).toArray(function(err ,result){
+    if (err) throw err;
+    if (result.length>0) {
+      res.status(200).json(result);
+    } else {
+      userTable.insert(req.body,function (err,r) {
+        if(err) throw err;
+        res.status(200).json(null);
+      })
+    }
+  });
+});
+
+
+
+
+app.listen(80);
